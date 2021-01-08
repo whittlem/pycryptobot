95cd503bb6f63d84d7181f51804ec661f9d5bab6
Michael Whittle <michael@lifecycle-ps.com>
2021-01-08 12:05:18 +0000

    several improvements

--- experiments.py
+++ experiments.py
@@ -3,6 +3,6 @@ import pandas as pd
 try:
     df = pd.read_csv('experiments/experiments.csv')
     print (df['result'].describe())
-    print (len(df[df['result'] >= 0]))
+    print (str(len(df[df['result'] >= 0]) / len(df) * 100) + '% success')
 except OSError:
     raise SystemExit('Unable to open: experiments/experiments.csv')
