ba39542ce5ad38aea37775e1abfb57ed453ca59e
Michael Whittle <michael@lifecycle-ps.com>
2021-01-14 23:17:17 +0000

    coinbase pro live methods added

---
+++ sandbox-coinbaseprolive.py
@@ -0,0 +1,25 @@
+import json
+from models.CoinbaseProAPI import CoinbaseProAPI
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+model = CoinbaseProAPI(config['api_key'], config['api_secret'], config['api_pass'], config['api_url'])
+
+accounts = model.getAccounts()
+print (accounts)
+
+#account = model.getAccount('00000000-0000-0000-0000-000000000000')
+#print (account)
+
+orders1 = model.getOrders()
+print (orders1)
+
+orders2 = model.getOrders('BTC-GBP')
+print (orders2)
+
+orders3 = model.getOrders('BTC-GBP', 'buy')
+print (orders3)
+
+orders4 = model.getOrders('BTC-GBP', 'buy', 'done')
+print (orders4)
