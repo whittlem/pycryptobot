94ea814d5b87876b3b712b4aca17d057b99e4be4
Michael Whittle <michael@lifecycle-ps.com>
2021-01-16 12:36:09 +0000

    saving progress

--- pycryptobot.py
+++ pycryptobot.py
@@ -1,7 +1,45 @@
 import pandas as pd
-import os, sched, sys, time
+import json, os, re, sched, sys, time
 from datetime import datetime
 from models.CoinbasePro import CoinbasePro
+from models.TradingAccount import TradingAccount
+from models.CoinbaseProAPI import CoinbaseProAPI
+
+"""
+DISCLAIMER:
+* Enable live trading (is_live = 1) completely at your own risk.
+* Set investment amount to all (invest_amount = -1) at your own risk.
+ * This will execute buys with your entire FIAT balance e.g. GBP
+ * This will execute sells with your entire crypto balance e.g. BTC
+"""
+
+"""
+Settings
+"""
+
+is_live = 1
+cryptoMarket = 'BTC'
+fiatMarket = 'GBP'
+granularity = 3600 # 1 hour
+
+"""
+Highly advisable not to change any code below this point!
+"""
+
+if cryptoMarket not in ['BCH','BTC','ETH','LTC']:
+    raise Exception('Invalid crypto market: BCH, BTC, ETH, LTC or ETH')
+
+if fiatMarket not in ['EUR','GBP','USD']:
+    raise Exception('Invalid FIAT market: EUR, GBP, USD')
+
+market = cryptoMarket + '-' + fiatMarket
+
+config = {}
+account = None
+if is_live == 1:
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+    account = TradingAccount(config)
 
 action = 'wait'
 last_action = ''
@@ -33,10 +71,23 @@ def executeJob(sc, market, granularity):
         print(df_last.index.format(), 'ema12:' + str(df_last['ema12'].values[0]), 'ema26:' + str(df_last['ema26'].values[0]), ema12gtema26co, ema12ltema26co, 'macd:' + str(df_last['macd'].values[0]), 'signal:' + str(df_last['signal'].values[0]), macdgtsignal, macdltsignal, obv_pc, action)
 
         if action == 'buy':
-            print ('>>> BUY <<<')
+            if is_live == 1:
+                print ('>>> EXECUTING LIVE BUY! <<<') 
+                model = CoinbaseProAPI(config['api_key'], config['api_secret'], config['api_pass'], config['api_url'])
+                resp = model.marketBuy(market, account.getBalance(fiatMarket))
+                print (resp)
+            else:
+                print ('>>> NON-LIVE SIMULATION BUY <<<')  
             print (df_last)
+            
         elif action == 'buy':
-            print ('>>> SELL <<<')
+            if is_live == 1:
+                print ('>>> EXECUTING LIVE SELL! <<<')
+                model = CoinbaseProAPI(config['api_key'], config['api_secret'], config['api_pass'], config['api_url'])
+                resp = model.marketSell(market, account.getBalance(cryptoMarket))
+                print (resp)                
+            else:
+                print ('>>> NON-LIVE SIMULATION SELL <<<')  
             print (df_last)
 
         last_action = action
@@ -45,9 +96,6 @@ def executeJob(sc, market, granularity):
     s.enter(300, 1, executeJob, (sc, market, granularity)) # poll every 5 minutes
 
 try:
-    market = 'BTC-GBP'
-    granularity = 3600 # 1 hour
-
     print("Python Crypto Bot using Coinbase Pro API\n")
     print(datetime.now(), 'started for market', market, 'using interval', granularity)
 
