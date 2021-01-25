e676f53696e668a2a7bdcf23939b8b6fb539f2fe
Michael Whittle <michael@lifecycle-ps.com>
2021-01-25 20:32:02 +0000

    added additional validation

--- pycryptobot.py
+++ pycryptobot.py
@@ -89,6 +89,12 @@ def executeJob(sc, market, granularity):
     coinbasepro.addMACDBuySignals()
     df = coinbasepro.getDataFrame()
 
+    if len(df) != 300:
+        # data frame should have 300 rows, if not retry
+        print('error: data frame length is < 300 (' + str(len(df)) + ')')
+        logging.error('error: data frame length is < 300 (' + str(len(df)) + ')')
+        s.enter(300, 1, executeJob, (sc, market, granularity))
+
     # df_last contains the most recent entry
     df_last = df.tail(1)
     ema12gtema26 = bool(df_last['ema12gtema26'].values[0])
@@ -102,6 +108,15 @@ def executeJob(sc, market, granularity):
     obv_pc = float(df_last['obv_pc'].values[0])
     obvsignal = bool(df_last['obvsignal'].values[0])
 
+    datetime_last = datetime.strptime(df_last.index.format()[0], '%Y-%m-%d %H:%M:%S')
+    datetime_diff_sec = (datetime.now() - datetime_last).total_seconds()
+
+    if datetime_diff_sec > granularity:
+        # last row timestamp needs to be current
+        print('error: out of date data, retry in 5 minutes')
+        logging.error('error: out of date date, retry in 5 minutes')
+        s.enter(300, 1, executeJob, (sc, market, granularity))
+    
     # criteria for a buy signal
     if ((ema12gtema26co == True and macdgtsignal == True and obv_pc >= 2) or (ema12gtema26 == True and macdgtsignal == True and obv_pc >= 5 and iterations >= 2)) and last_action != 'BUY':
         action = 'BUY'
