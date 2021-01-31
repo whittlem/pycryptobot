f9ede6c0e616b8bec6e622f054cfc8d418872aa6
Michael Whittle <michael@lifecycle-ps.com>
2021-01-31 12:40:14 +0000

    added sandbox-getbalances.py

---
+++ sandox-getbalances.py
@@ -0,0 +1,8 @@
+import json
+from models.TradingAccount import TradingAccount
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+account = TradingAccount(config)
+print (account.getBalance())
