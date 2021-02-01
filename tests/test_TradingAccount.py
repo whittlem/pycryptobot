c54f4173a1842c5eea7e9daf5b07cc2bb6ab66b0
Michael Whittle <michael@lifecycle-ps.com>
2021-02-01 15:15:05 +0000

    started adding unit tests

---
+++ tests/test_TradingAccount.py
@@ -0,0 +1,33 @@
+import pytest, sys
+
+sys.path.append('.')
+# pylint: disable=import-error
+from models.TradingAccount import TradingAccount
+
+def test_default_initial_balance():
+    account = TradingAccount()
+    assert account.getBalance('GBP') == 1000
+
+def test_buy_sufficient_funds():
+    account = TradingAccount()
+    account.buy('BTC', 'GBP', 1000, 30000)
+    assert account.getBalance('GBP') == 0
+
+def test_buy_insufficient_funds():
+    account = TradingAccount()
+    with pytest.raises(Exception) as execinfo:
+        account.buy('BTC', 'GBP', 1001, 30000)
+    assert str(execinfo.value) == 'Insufficient funds.'
+
+def test_sell_insufficient_funds():
+    account = TradingAccount()
+    account.buy('BTC', 'GBP', 1000, 30000)
+    with pytest.raises(Exception) as execinfo:
+        account.sell('BTC', 'GBP', 1, 35000)
+    assert str(execinfo.value) == 'Insufficient funds.'
+
+def test_successful_buy_and_sell():
+    account = TradingAccount()
+    account.buy('BTC', 'GBP', 1000, 30000)
+    account.sell('BTC', 'GBP', 0.033167, 35000)
+    assert account.getBalance() == 1155.04
