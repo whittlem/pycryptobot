7572a846b1dcd90bffa36c8f68be9dc0566f4802
Michael Whittle <michael@lifecycle-ps.com>
2021-02-04 19:41:29 +0000

    updated buy and sell signals

--- simulations.py
+++ simulations.py
@@ -81,8 +81,8 @@ def runExperiment(id, market='BTC-GBP', granularity=3600, mostRecent=True):
     df = technicalAnalysis.getDataFrame()
 
     # defines the buy and sell signals and consolidates into df_signals
-    buysignals = ((df.ema12gtema26co == True) & (df.macdgtsignal == True) & (df.obv_pc > 0)) | ((df.ema12gtema26 == True) & (df.ema12gtema26 == True) & (df.macdgtsignal == True) & (df.obv_pc >= 2))
-    sellsignals = (((df.ema12ltema26co == True) & (df.macdltsignal == True)) | ((df.ema12gtema26 == True) & ((df.macdltsignal == True) & (df.obv_pc < 0))))
+    buysignals = ((df.ema12gtema26co == True & df.macdgtsignal == True & df.obv_pc > 0.1) | (df.ema12gtema26 == True & df.macdgtsignal == True & df.obv_pc >= 2))
+    sellsignals = (df.ema12ltema26co == True & df.macdltsignal == True)
     df_signals = df[(buysignals) | (sellsignals)]
 
     diff = 0
