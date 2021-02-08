6aa987a6129f641ffdf6a98ece0f1a0ff916f800
Michael Whittle <michael@lifecycle-ps.com>
2021-02-08 17:34:24 +0000

    fixed passphrase bug

--- models/CoinbasePro.py
+++ models/CoinbasePro.py
@@ -58,7 +58,7 @@ class AuthAPI():
         # validates the api passphase is syntactically correct
         p = re.compile(r"^[a-z0-9]{10,11}$")
         if not p.match(api_pass):
-            err = 'Coinbase Pro API passphase is invalid'
+            err = 'Coinbase Pro API passphrase is invalid'
             if self.debug:
                 raise TypeError(err)
             else:
