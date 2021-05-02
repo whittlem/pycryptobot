bec2e3f488daf367fb84d4415de82dbc362dd738
Michael Whittle <michael@lifecycle-ps.com>
2021-05-02 16:23:17 +0100

    adding additional pytests

--- tests/test_Exchange_CoinbasePro.py
+++ tests/test_Exchange_CoinbasePro.py
@@ -1,4 +1,4 @@
-import pytest, sys
+import json, pandas, pytest, os, sys
 
 sys.path.append('.')
 # pylint: disable=import-error
@@ -51,3 +51,102 @@ def test_instantiate_authapi_with_api_url_error():
 def test_instantiate_publicapi_without_error():
     exchange = PublicAPI()
     assert type(exchange) is PublicAPI
+
+def test_config_json_exists_and_valid():
+    filename = 'config.json'
+    assert os.path.exists(filename) == True
+    with open(filename) as config_file:
+        config = json.load(config_file)
+
+        api_key = ''
+        api_secret = ''
+        api_passphrase = ''
+        api_url = ''
+        if 'api_key' in config and 'api_secret' in config and 'api_pass' in config and 'api_url' in config:
+            api_key = config['api_key']
+            api_secret = config['api_secret']
+            api_passphrase = config['api_pass']
+            api_url = config['api_url']
+            AuthAPI(api_key, api_secret, api_passphrase, api_url)
+        elif 'api_key' in config['coinbasepro'] and 'api_secret' in config['coinbasepro'] and 'api_passphrase' in config['coinbasepro'] and 'api_url' in config['coinbasepro']:
+            api_key = config['coinbasepro']['api_key']
+            api_secret = config['coinbasepro']['api_secret']
+            api_passphrase = config['coinbasepro']['api_passphrase']
+            api_url = config['coinbasepro']['api_url']
+            AuthAPI(api_key, api_secret, api_passphrase, api_url)
+    pass
+
+def test_getAccounts():
+    filename = 'config.json'
+
+    with open(filename) as config_file:
+        config = json.load(config_file)
+
+        api_key = ''
+        api_secret = ''
+        api_passphrase = ''
+        api_url = ''
+        if 'api_key' in config and 'api_secret' in config and 'api_pass' in config and 'api_url' in config:
+            api_key = config['api_key']
+            api_secret = config['api_secret']
+            api_passphrase = config['api_pass']
+            api_url = config['api_url']
+            
+        elif 'api_key' in config['coinbasepro'] and 'api_secret' in config['coinbasepro'] and 'api_passphrase' in config['coinbasepro'] and 'api_url' in config['coinbasepro']:
+            api_key = config['coinbasepro']['api_key']
+            api_secret = config['coinbasepro']['api_secret']
+            api_passphrase = config['coinbasepro']['api_passphrase']
+            api_url = config['coinbasepro']['api_url']
+
+    exchange = AuthAPI(api_key, api_secret, api_passphrase, api_url)
+    assert type(exchange) is AuthAPI
+
+    df = exchange.getAccounts()
+    assert type(df) is pandas.core.frame.DataFrame
+
+    actual = df.columns.to_list()
+    expected = [ 'index', 'id', 'currency', 'balance', 'hold', 'available', 'profile_id', 'trading_enabled' ]
+    assert len(actual) == len(expected)
+    assert all([a == b for a, b in zip(actual, expected)])
+
+def test_getAccount():
+    filename = 'config.json'
+
+    with open(filename) as config_file:
+        config = json.load(config_file)
+
+        api_key = ''
+        api_secret = ''
+        api_passphrase = ''
+        api_url = ''
+        if 'api_key' in config and 'api_secret' in config and 'api_pass' in config and 'api_url' in config:
+            api_key = config['api_key']
+            api_secret = config['api_secret']
+            api_passphrase = config['api_pass']
+            api_url = config['api_url']
+            
+        elif 'api_key' in config['coinbasepro'] and 'api_secret' in config['coinbasepro'] and 'api_passphrase' in config['coinbasepro'] and 'api_url' in config['coinbasepro']:
+            api_key = config['coinbasepro']['api_key']
+            api_secret = config['coinbasepro']['api_secret']
+            api_passphrase = config['coinbasepro']['api_passphrase']
+            api_url = config['coinbasepro']['api_url']
+
+    exchange = AuthAPI(api_key, api_secret, api_passphrase, api_url)
+    assert type(exchange) is AuthAPI
+
+    df = exchange.getAccounts()
+
+    assert len(df) > 0
+
+    account = df[['id']].head(1).values[0][0]
+    assert len(account) > 0
+
+    df = exchange.getAccount(account)
+    assert type(df) is pandas.core.frame.DataFrame
+
+    assert len(df) == 1
+
+    actual = df.columns.to_list()
+    expected = [ 'id', 'currency', 'balance', 'hold', 'available', 'profile_id', 'trading_enabled' ]
+    assert len(actual) == len(expected)
+    assert all([a == b for a, b in zip(actual, expected)])
