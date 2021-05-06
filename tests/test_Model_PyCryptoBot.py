6573095dfb198ff1fb1f776c62eb401b40957309
Michael Whittle <michael@lifecycle-ps.com>
2021-05-06 23:01:11 +0100

    adding unit tests

--- tests/test_Model_PyCryptoBot.py
+++ tests/test_Model_PyCryptoBot.py
@@ -617,9 +617,7 @@ def test_configjson_islive():
             "api_key" : "00000000000000000000000000000000",
             "api_secret" : "0000/0000000000/0000000000000000000000000000000000000000000000000000000000/00000000000==",
             "api_passphrase" : "00000000000",
-            "config" : {
-                "live" : 1
-            }
+            "config" : {}
         } 
     }
 
@@ -634,10 +632,105 @@ def test_configjson_islive():
     app = PyCryptoBot(filename='/tmp/pycryptobot_pytest_config.json')
     assert type(app) is PyCryptoBot
     assert app.getExchange() == 'coinbasepro'
+    assert app.isLive() == 0
+
+    try:
+        config['coinbasepro']['config']['live'] = 1
+        config_json = json.dumps(config)
+        fh = open('/tmp/pycryptobot_pytest_config.json', 'w')
+        fh.write(config_json)
+        fh.close()
+    except Exception as err:
+        print (err)
+
+    app = PyCryptoBot(filename='/tmp/pycryptobot_pytest_config.json')
+    assert type(app) is PyCryptoBot
     assert app.isLive() == 1
 
     app.setLive(0)
     assert app.isLive() == 0
+
+    if os.path.exists('/tmp/pycryptobot_pytest_config.json'):
+        os.remove('/tmp/pycryptobot_pytest_config.json')
+
+def test_configjson_graphs():
+    config = {
+        "coinbasepro" : {
+            "api_url" : "https://api.pro.coinbase.com",
+            "api_key" : "00000000000000000000000000000000",
+            "api_secret" : "0000/0000000000/0000000000000000000000000000000000000000000000000000000000/00000000000==",
+            "api_passphrase" : "00000000000",
+            "config" : {}
+        } 
+    }
+
+    try:
+        config_json = json.dumps(config)
+        fh = open('/tmp/pycryptobot_pytest_config.json', 'w')
+        fh.write(config_json)
+        fh.close()
+    except Exception as err:
+        print (err)
+
+    app = PyCryptoBot(filename='/tmp/pycryptobot_pytest_config.json')
+    assert type(app) is PyCryptoBot
+    assert app.getExchange() == 'coinbasepro'
+    assert app.shouldSaveGraphs() == 0
+
+    try:
+        config['coinbasepro']['config']['graphs'] = 0
+        config_json = json.dumps(config)
+        fh = open('/tmp/pycryptobot_pytest_config.json', 'w')
+        fh.write(config_json)
+        fh.close()
+    except Exception as err:
+        print (err)
+
+    app = PyCryptoBot(filename='/tmp/pycryptobot_pytest_config.json')
+    assert type(app) is PyCryptoBot
+    assert app.getExchange() == 'coinbasepro'
+    assert app.shouldSaveGraphs() == 0
+
+    if os.path.exists('/tmp/pycryptobot_pytest_config.json'):
+        os.remove('/tmp/pycryptobot_pytest_config.json')
+
+def test_configjson_isverbose():
+    config = {
+        "coinbasepro" : {
+            "api_url" : "https://api.pro.coinbase.com",
+            "api_key" : "00000000000000000000000000000000",
+            "api_secret" : "0000/0000000000/0000000000000000000000000000000000000000000000000000000000/00000000000==",
+            "api_passphrase" : "00000000000",
+            "config" : {}
+        } 
+    }
+
+    try:
+        config_json = json.dumps(config)
+        fh = open('/tmp/pycryptobot_pytest_config.json', 'w')
+        fh.write(config_json)
+        fh.close()
+    except Exception as err:
+        print (err)
+
+    app = PyCryptoBot(filename='/tmp/pycryptobot_pytest_config.json')
+    assert type(app) is PyCryptoBot
+    assert app.getExchange() == 'coinbasepro'
+    assert app.isVerbose() == 0
+
+    try:
+        config['coinbasepro']['config']['verbose'] = 1
+        config_json = json.dumps(config)
+        fh = open('/tmp/pycryptobot_pytest_config.json', 'w')
+        fh.write(config_json)
+        fh.close()
+    except Exception as err:
+        print (err)
+
+    app = PyCryptoBot(filename='/tmp/pycryptobot_pytest_config.json')
+    assert type(app) is PyCryptoBot
+    assert app.getExchange() == 'coinbasepro'
+    assert app.isVerbose() == 1
 
     if os.path.exists('/tmp/pycryptobot_pytest_config.json'):
         os.remove('/tmp/pycryptobot_pytest_config.json')
