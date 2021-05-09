8d5338ce4cf1ab56f7882f10b2e67cf6c03c9662
Michael Whittle <michael@lifecycle-ps.com>
2021-05-09 12:00:23 +0100

    replace global variables with state class

---
+++ models/AppState.py
@@ -0,0 +1,19 @@
+"""Application state class"""
+
+class AppState():
+    def __init__(self):
+        self.action = 'WAIT'
+        self.buy_count = 0
+        self.buy_state = ''
+        self.buy_sum = 0
+        self.eri_text = ''
+        self.fib_high = 0
+        self.fib_low = 0
+        self.iterations = 0
+        self.last_action = ''
+        self.last_buy_price = 0
+        self.last_buy_amount = 0
+        self.last_buy_high = 0 
+        self.last_df_index = ''
+        self.sell_count = 0
+        self.sell_sum = 0
