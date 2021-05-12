268b9c79eef9c4167c9a32cb0123a9077cc36aa8
Michael Whittle <michael@lifecycle-ps.com>
2021-05-12 17:33:37 +0100

    last_action bugfix

--- pycryptobot.py
+++ pycryptobot.py
@@ -61,20 +61,25 @@ elif app.isLive() == 1:
             raise Exception('Insufficient available funds to place buy order: ' + str(account.getBalance(app.getQuoteCurrency())) + ' < 50 ' + app.getQuoteCurrency() + "\nNote: A manual limit order places a hold on available funds.")
         elif state.last_action == 'BUY' and account.getBalance(app.getBaseCurrency()) < 0.001:
             raise Exception('Insufficient available funds to place sell order: ' + str(account.getBalance(app.getBaseCurrency())) + ' < 0.1 ' + app.getBaseCurrency() + "\nNote: A manual limit order places a hold on available funds.")
-
+    print (state.last_action, account.getBalance(app.getQuoteCurrency()))
     orders = account.getOrders(app.getMarket(), '', 'done')
     if len(orders) > 0:
         df = orders[-1:]
 
         if str(df.action.values[0]) == 'buy':
-            state.last_action = 'BUY'
+            #state.last_action = 'BUY'
             state.last_buy_size = float(df[df.action == 'buy']['size'])
             state.last_buy_filled = float(df[df.action == 'buy']['filled'])
             state.last_buy_price = float(df[df.action == 'buy']['price'])
-            state.last_buy_fee = round(float(df[df.action == 'buy']['filled']) * float(df[df.action == 'buy']['price']) * app.getTakerFee(), 2)
+            state.last_buy_fee = round(state.last_buy_filled * state.last_buy_price * app.getTakerFee(), 2)
         else:
-            state.last_action = 'SELL'
+            #state.last_action = 'SELL'
             state.last_buy_price = 0.0
+
+        print (df)
+
+print (state.last_action)
+sys.exit()
 
 def calculateMargin(buy_size: float=0.0, buy_filled: int=0.0, buy_price: int=0.0, buy_fee: float=0.0, sell_percent: float=100, sell_price: float=0.0, sell_fee: float=0.0, sell_taker_fee: float=0.0, debug: bool=False) -> float:
     if debug is True:
