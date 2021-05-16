9b8b4656823296389eab91f3dfbce766f3e2a8d9
marioK93 <38282614+marioK93@users.noreply.github.com>
2021-05-16 12:36:35 +0100

    update margin comments to use base/quote

--- models/helper/MarginHelper.py
+++ models/helper/MarginHelper.py
@@ -3,43 +3,43 @@ def calculate_margin(buy_size: float = 0.0, buy_filled: int = 0.0, buy_price: in
                      sell_taker_fee: float = 0.0, debug: bool = False, exchange: str = 'coinbasepro') -> float:
     
     if debug is True:
-        print(f'buy_size: {buy_size}') #buy_size for CB is FIAT, for binance is crypto
-        print(f'buy_filled: {buy_filled}') #buy_filled for CB is Crypto, for binance is FIAT
-        print(f'buy_price: {buy_price}') #buy_price for CB is FIAT, for binance is FIAT
-        print(f'buy_fee: {buy_fee}', "\n") #buy_fee for CB is FIAT, for binance is FIAT
+        print(f'buy_size: {buy_size}') #buy_size for CB is quote currency, for binance is base currency
+        print(f'buy_filled: {buy_filled}') #buy_filled for CB is base currency, for binance is quote currency
+        print(f'buy_price: {buy_price}') #buy_price for CB is quote currency, for binance is quote currency
+        print(f'buy_fee: {buy_fee}', "\n") #buy_fee for CB is quote currency, for binance is quote currency
 
-    #for CB buy_size represents the FIAT value of the buy including fees and buy_filled represents the Crypto size of the buy
-    #for Binance buy_filled represents the FIAT value of the buy including fees and buy_size represents the Crypto size of the buy
+    #for CB buy_size represents the quote currency value of the buy including fees and buy_filled represents the base currency size of the buy
+    #for Binance buy_filled represents the quote currency value of the buy including fees and buy_size represents the base currency size of the buy
 
     if exchange == 'coinbasepro':
-        #sell_size in FIAT by multiplying current price by buy_filled in Crypto
+        #sell_size in quote currency by multiplying current price by buy_filled in base currency
         sell_size = round((sell_percent / 100) * (sell_price  * buy_filled), 2)
 
-        #calculate sell_fee in FIAT
+        #calculate sell_fee in quote currency
         if sell_fee == 0.0 and sell_taker_fee > 0.0:
             sell_fee = round((sell_size * sell_taker_fee), 2)
         
-        #calculate sell_value after fees in FIAT
+        #calculate sell_value after fees in quote currency
         sell_value = round(sell_size - sell_fee, 2)
 
-        #profit is difference between sell_value without fees and buy_size including fees in FIAT
+        #profit is difference between sell_value without fees and buy_size including fees in quote currency
         profit = round(sell_value - buy_size, 2)
 
         #calculate margin
         margin = round((profit / buy_size) * 100, 2)
 
     else:
-        #sell size in FIAT by multiplying current price by buy_size in Crypto
+        #sell size in quote currency by multiplying current price by buy_size in base currency
         sell_size = (sell_percent / 100) * (sell_price  * buy_size)
 
-        #calculate sell fee in FIAT
+        #calculate sell fee in quote currency
         if sell_fee == 0.0 and sell_taker_fee > 0.0:
             sell_fee = (sell_size /100) * sell_taker_fee
         
-        #calculate sell_value after fees in FIAT
+        #calculate sell_value after fees in quote currency
         sell_value = sell_size - sell_fee
         
-        #profit is difference between sell_value and buy_filled in FIAT
+        #profit is difference between sell_value and buy_filled in quote currency
         profit = round(sell_value - buy_filled, 2)
         
         #calculate margin
