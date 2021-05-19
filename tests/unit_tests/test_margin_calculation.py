4293f8170f89b6ec215ace878f78c88d62c50bad
Emre Eroglu <emre@emreeroglu.net>
2021-05-19 20:55:18 +0300

    Fix precision at testcases.

--- tests/unit_tests/test_margin_calculation.py
+++ tests/unit_tests/test_margin_calculation.py
@@ -6,24 +6,32 @@ from models.helper.MarginHelper import calculate_margin
 
 def test_margin_binance():
     # using VET-GBP
-    buy_fee = 0.07434422700000001
-    buy_filled = 74.344227
-    buy_price = 0.1249
     buy_size = 595.23
+    buy_fee = 0.59523  # round(buy_size * 0.001, 8)
+    buy_price = 0.1249
+    buy_filled = 74.344227 # round(buy_size * buy_price, 8)
+        
     debug = True
-    exchange = 'binance'
-    sell_fee = 0.0
     sell_percent = 100
     sell_price = 0.1335
+    sell_size = 79.463205 # round((sell_percent / 100) * (sell_price * buy_size), 8)
     sell_taker_fee = 0.001
+    sell_filled = 79.38374179 # round((sell_size - round(sell_size * sell_taker_fee, 8)), 8)
+    sell_fee = 0.0
+    exchange = 'binance'
+    # round(((buy_size - buy_fee) / buy_price), 8)
+
+    expected_sell_fee = 0.07946321 # round(sell_size * sell_taker_fee, 8)
+    expected_profit = 5.03951479 # round(sell_filled - buy_filled, 8)
+    expected_margin = 6.77862289 # round((expected_profit / buy_filled) * 100, 8)
 
     actual_margin, actual_profit, actual_sell_fee = calculate_margin(buy_size, buy_filled, buy_price, buy_fee,
                                                                      sell_percent, sell_price, sell_fee, sell_taker_fee,
                                                                      debug, exchange)
 
-    assert actual_margin == 6.78
-    assert actual_profit == 5.04
-    assert actual_sell_fee == 0.07946321
+    assert round(actual_margin, 8) == round(expected_margin, 8)
+    assert round(actual_profit, 8) == round(expected_profit, 8)
+    assert round(actual_sell_fee, 8) == round(expected_sell_fee, 8)
 
 
 def test_margin_coinbase_pro():
@@ -59,20 +67,20 @@ def test_calculate_negative_margin_on_binance_when_coin_price_over_1():
     sell_price = 590.34
     sell_size = 39.55278 # round((sell_percent / 100) * (sell_price * buy_size), 8)
     sell_taker_fee = 0.001
-    sell_filled = 39.51322722 # round((sell_size - (sell_size * sell_taker_fee)), 8)
+    sell_filled = 39.51322722 # round((sell_size - round(sell_size * sell_taker_fee, 8)), 8)
     sell_fee = 0.0
     exchange = 'binance'
 
     expected_sell_fee = 0.03955278 # round(sell_size * sell_taker_fee, 8)
-    expected_profit = -5.19 # round(sell_filled - buy_filled, 2)
-    expected_margin = -11.61 # round((expected_profit / buy_filled) * 100, 2)
+    expected_profit = -5.19319278 # round(sell_filled - buy_filled, 8)
+    expected_margin = -11.61621257 # round((expected_profit / buy_filled) * 100, 8)
 
     actual_margin, actual_profit, actual_sell_fee = calculate_margin(buy_size, buy_filled, buy_price, buy_fee,
                                                                      sell_percent, sell_price, sell_fee, sell_taker_fee,
                                                                      debug, exchange)
 
-    assert round(actual_margin, 2) == round(expected_margin, 2)
-    assert round(actual_profit, 2) == round(expected_profit, 2)
+    assert round(actual_margin, 8) == round(expected_margin, 8)
+    assert round(actual_profit, 8) == round(expected_profit, 8)
     assert round(actual_sell_fee, 8) == round(expected_sell_fee, 8)
 
 
@@ -86,20 +94,20 @@ def test_calculate_negative_margin_on_binance_when_coin_price_over_1_2():
     sell_price = 320.95
     sell_size = 80.057768 # round((sell_percent / 100) * (sell_price * buy_size), 8)
     sell_taker_fee = 0.001
-    sell_filled = 79.97771023 # round((sell_size - (sell_size * sell_taker_fee)), 8)
+    sell_filled = 79.97771023 # round((sell_size - round(sell_size * sell_taker_fee, 8)), 8)
     sell_fee = 0.0
     exchange = 'binance'
 
     expected_sell_fee = 0.08005777 # round(sell_size * sell_taker_fee, 8)
-    expected_profit = -16.16 # round(sell_filled - buy_filled, 2)
-    expected_margin = -16.81 # round((expected_profit / buy_filled) * 100, 2)
+    expected_profit = -16.15646577 # round(sell_filled - buy_filled, 8)
+    expected_margin = -16.80616243 # round((expected_profit / buy_filled) * 100, 8)
 
     actual_margin, actual_profit, actual_sell_fee = calculate_margin(buy_size, buy_filled, buy_price, buy_fee,
                                                                      sell_percent, sell_price, sell_fee, sell_taker_fee,
                                                                      debug, exchange)
 
-    assert round(actual_margin, 2) == round(expected_margin, 2)
-    assert round(actual_profit, 2) == round(expected_profit, 2)
+    assert round(actual_margin, 8) == round(expected_margin, 8)
+    assert round(actual_profit, 8) == round(expected_profit, 8)
     assert round(actual_sell_fee, 8) == round(expected_sell_fee, 8)
 
 def test_calculate_negative_margin_on_binance_when_coin_price_under_1():
@@ -114,18 +122,18 @@ def test_calculate_negative_margin_on_binance_when_coin_price_under_1():
     sell_price = 0.43913 
     sell_size = 77.813836 # round((sell_percent / 100) * (sell_price * buy_size), 8)
     sell_taker_fee = 0.001
-    sell_filled = 77.73602216  # round((sell_size - (sell_size * sell_taker_fee)), 8) 
+    sell_filled = 77.73602216  # round((sell_size - round(sell_size * sell_taker_fee, 8)), 8)
     sell_fee = 0.0
     exchange = 'binance'
 
     expected_sell_fee = 0.07781384 # round(sell_size * sell_taker_fee, 8)
-    expected_profit = -10.31 # round(sell_filled - buy_filled, 2)
-    expected_margin = -11.71 # round((expected_profit / buy_filled) * 100, 2)
+    expected_profit = -10.30756984 # round(sell_filled - buy_filled, 8)
+    expected_margin = -11.70734815 # round((expected_profit / buy_filled) * 100, 8)
 
     actual_margin, actual_profit, actual_sell_fee = calculate_margin(buy_size, buy_filled, buy_price, buy_fee,
                                                                      sell_percent, sell_price, sell_fee, sell_taker_fee,
                                                                      debug, exchange)
 
-    assert round(actual_margin, 2) == round(expected_margin, 2)
-    assert round(actual_profit, 2) == round(expected_profit, 2)
+    assert round(actual_margin, 8) == round(expected_margin, 8)
+    assert round(actual_profit, 8) == round(expected_profit, 8)
     assert round(actual_sell_fee, 8) == round(expected_sell_fee, 8)
