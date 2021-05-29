1daf53031258bb742d4bf04458a8add8cfe06a5d
Emre Eroglu <emreeroglu29@gmail.com>
2021-05-30 02:09:40 +0300

    Add logger_parser.

--- models/config/__init__.py
+++ models/config/__init__.py
@@ -1,3 +1,4 @@
 from .binance_parser import parser as binanceConfigParser, parseMarket as binanceParseMarket
 from .coinbase_pro_parser import parser as coinbaseProConfigParser, parseMarket as coinbaseProParseMarket
 from .dummy_parser import parser as dummyConfigParser, parseMarket as dummyParseMarket
+from .logger_parser import parser as loggerConfigParser
---
+++ models/config/logger_parser.py
@@ -0,0 +1,54 @@
+import re
+
+from .default_parser import isCurrencyValid, defaultConfigParse, merge_config_and_args
+from models.helper.LogHelper import Logger
+
+def parser(app, logger_config):
+    print('Logger Configuration parser')
+
+    if not logger_config:
+        raise Exception('There is an error in your config dictionnary')
+
+    if not app:
+        raise Exception('No app is passed')
+
+    if 'filelog' in logger_config:
+        if isinstance(logger_config['filelog'], int):
+            if logger_config['filelog'] in [0, 1]:
+                app.filelog = logger_config['filelog']
+        else:
+            raise TypeError('filelog must be type of int')
+
+    if app.filelog:
+        if 'logfile' in logger_config:
+            if isinstance(logger_config['logfile'], str):
+                if app.logfile == "pycryptobot.log":
+                    app.logfile = logger_config['logfile']   
+            else:
+                raise TypeError('logfile must be type of str')
+
+        if 'fileloglevel' in logger_config:
+            if isinstance(logger_config['fileloglevel'], str):
+                if logger_config['fileloglevel'] in ('CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET'):
+                    app.fileloglevel = logger_config['fileloglevel']   
+                else:
+                    raise TypeError('fileloglevel must be one of: "CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", "NOTSET"')
+            else:
+                raise TypeError('fileloglevel must be type of str')
+        
+    if 'consolelog' in logger_config:
+        if isinstance(logger_config['consolelog'], int):
+            if logger_config['consolelog'] in [0, 1]:
+                app.consolelog = logger_config['consolelog']
+        else:
+            raise TypeError('consolelog must be type of int')
+
+    if app.consolelog:
+        if 'consoleloglevel' in logger_config:
+            if isinstance(logger_config['consoleloglevel'], str):
+                if logger_config['consoleloglevel'] in ('CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET'):
+                    app.consoleloglevel = logger_config['consoleloglevel']   
+                else:
+                    raise TypeError('consoleloglevel must be one of: "CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", "NOTSET"')       
+            else:
+                raise TypeError('consoleloglevel must be type of str')
