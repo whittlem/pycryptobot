edaacf0173cc77fb4d8ead39633630c1d08324c6
markhollingworth-worthit <61147577+markhollingworth-worthit@users.noreply.github.com>
2021-07-03 17:54:49 +0100

    bug for for smartswitch

--- pycryptobot.py
+++ pycryptobot.py
@@ -55,12 +55,13 @@ def executeJob(sc=None, app: PyCryptoBot=None, state: AppState=None, trading_dat
             return None
 
     # analyse the market data
-    if app.isSimulation() and len(trading_data.columns) > 8:
+    if app.isSimulation() and len(trading_data.columns) > 8:    
         df = trading_data
         # if smartswitch the get the market data using new granularity
         if app.sim_smartswitch:
             df_last = app.getInterval(df, state.iterations)
             if len(df_last.index.format()) > 0:
+
                 current_df_index = str(df_last.index.format()[0])
                 current_sim_date = f'{current_df_index} 00:00:00' if len(current_df_index) == 10 else current_df_index
                 dt = current_sim_date.split(' ')
@@ -99,7 +100,7 @@ def executeJob(sc=None, app: PyCryptoBot=None, state: AppState=None, trading_dat
     if app.getSmartSwitch() == 1 and app.getGranularity() == 3600 and app.is1hEMA1226Bull(current_sim_date) is True and app.is6hEMA1226Bull(current_sim_date) is True:
         Logger.info('*** smart switch from granularity 3600 (1 hour) to 900 (15 min) ***')
         
-        if app.sim_smartswitch:
+        if app.isSimulation():
             app.sim_smartswitch = True
         
         app.notifyTelegram(app.getMarket() + " smart switch from granularity 3600 (1 hour) to 900 (15 min)")
@@ -112,7 +113,7 @@ def executeJob(sc=None, app: PyCryptoBot=None, state: AppState=None, trading_dat
     if app.getSmartSwitch() == 1 and app.getGranularity() == 900 and app.is1hEMA1226Bull(current_sim_date) is False and app.is6hEMA1226Bull(current_sim_date) is False:
         Logger.info("*** smart switch from granularity 900 (15 min) to 3600 (1 hour) ***")
         
-        if app.sim_smartswitch:
+        if app.isSimulation():
             app.sim_smartswitch = True
         
         app.notifyTelegram(app.getMarket() + " smart switch from granularity 900 (15 min) to 3600 (1 hour)")
