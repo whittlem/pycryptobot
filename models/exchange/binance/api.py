021449a8d9eca7e2f71fe258028b42b6c75bfb69
Michael Whittle <michael@lifecycle-ps.com>
2021-08-17 22:32:58 +0100

    removed debug print

--- models/exchange/binance/api.py
+++ models/exchange/binance/api.py
@@ -823,7 +823,6 @@ class PublicAPI(AuthAPIBase):
             raise ValueError("Binance API URL is invalid")
 
         self._api_url = api_url
-        print (self._api_url)
 
     def getTime(self) -> datetime:
         """Retrieves the exchange time"""
