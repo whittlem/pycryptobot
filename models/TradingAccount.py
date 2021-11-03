639d39e605c665db2f17491a121cd153181e9bbe
Maks Buriy <maximb@netvertise.com>
2021-11-03 19:02:12 +0200

    Fix Binance Market validation

--- models/TradingAccount.py
+++ models/TradingAccount.py
@@ -61,7 +61,7 @@ class TradingAccount:
             if not p.match(market):
                 raise TypeError("Coinbase Pro market is invalid.")
         elif self.app.getExchange() == "binance":
-            p = re.compile(r"^[A-Z]{5,12}$")
+            p = re.compile(r"^[1-9A-Z]{5,12}$")
             if not p.match(market):
                 raise TypeError("Binance market is invalid.")
         elif self.app.getExchange() == "kucoin":
