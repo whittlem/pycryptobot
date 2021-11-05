08465812053ef0e36705ba22d74080bd93cd72ae
Édrick Renan <edrickrenan@gmail.com>
2021-11-05 10:22:30 +0000

    Adding test to assert exception when invalid value is passed
    Signed-off-by: edrick <edrickrenan@gmail.com>

--- tests/unit_tests/test_granularity_enum.py
+++ tests/unit_tests/test_granularity_enum.py
@@ -1,5 +1,7 @@
 import sys
 
+import pytest
+
 from models.exchange.Granularity import Granularity
 
 sys.path.append('.')
@@ -10,3 +12,10 @@ def test_enum_can_be_created_from_values():
     assert Granularity.convert_to_enum("1m") == Granularity.ONE_MINUTE
     assert Granularity.convert_to_enum("1min") == Granularity.ONE_MINUTE
     assert Granularity.convert_to_enum("1T") == Granularity.ONE_MINUTE
+
+
+def test_exception_thrown_when_invalid_value():
+    with pytest.raises(ValueError) as exc_info:
+        Granularity(10000000)
+    assert exc_info.type is ValueError
+    assert exc_info.value.args[0] == "10000000 is not a valid Granularity"
