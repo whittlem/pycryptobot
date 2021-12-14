6ee7292d0a6f634d71fb55d328d762186cdc24a9
markhollingworth-worthit <61147577+markhollingworth-worthit@users.noreply.github.com>
2021-12-14 17:14:18 +0000

    margin text update

--- models/telegram/actions.py
+++ models/telegram/actions.py
@@ -27,7 +27,7 @@ class TelegramActions():
         result = f"{light_icon} <b>{market}</b>\n" \
                     f"{margin_icon} Margin: {self.helper.data['margin']}  " \
                     f"\U0001F4B0 P/L: {self.helper.data['delta']}\n" \
-                    f"TSL Trg: {self.helper.data['trailingstoplosstriggered']}\n" \
+                    f"TSL Trg: {self.helper.data['trailingstoplosstriggered']}  " \
                     f"TSL Change: {float(self.helper.data['change_pcnt_high']).__round__(4)}\n"
         return result
 
