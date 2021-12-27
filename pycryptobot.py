ce645bd62e2059f32c982fd82899cdc41027e605
Chris Koepf <chriskoepf@users.noreply.github.com>
2021-12-26 22:28:10 -0500

    Fix isSellTrigger and revise change_pcnt_high fix
    - revised fix for change_pcnt_high variable from previous
    - fixed isSellTrigger immediate action it was waiting until candle close

--- pycryptobot.py
+++ pycryptobot.py
@@ -386,7 +386,6 @@ def executeJob(
                 )
     # change_pcnt_high set to 0 here to prevent errors on some tokens for some users.
     # Need to track down main source of error.  This allows bots to launch in those instances.
-    change_pcnt_high = 0
     if len(df_last) > 0:
         now = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -560,7 +559,7 @@ def executeJob(
                 immediate_action = True
 
         # handle overriding wait actions (e.g. do not sell if sell at loss disabled!, do not buy in bull if bull only)
-        if strategy.isWaitTrigger(_app, margin, goldencross):
+        if not immediate_action and strategy.isWaitTrigger(_app, margin, goldencross):
             _state.action = "WAIT"
             immediate_action = False
 
