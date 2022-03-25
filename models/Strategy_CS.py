902939a3ffc2c52289d8046c8c85e6afb769e16e
Chris Koepf <chriskoepf@users.noreply.github.com>
2022-03-25 19:29:10 -0400

    update Changelog, minor adjustments
    update Changelog
    minor Strategy adjustments to reduce risk initially

--- models/Strategy_CS.py
+++ models/Strategy_CS.py
@@ -32,7 +32,7 @@ class Strategy_CS:
         # total points required to buy
         self.pts_to_buy = 8 # more points requires more signals to activate and less risky
         # total points to trigger immediate buy pcnt if configured, if not, this is ignored
-        self.immed_buy_pts = 10
+        self.immed_buy_pts = 11
 
         # total points required to sell
         self.pts_to_sell = 3 # only requiring a couple pts will get sell signal quicker
@@ -41,10 +41,10 @@ class Strategy_CS:
 
         # Required signals.
         # Specify how many have to be triggered
-        # Buys - currently, RSI, OBV, MACDL - add self.pts_sig_required_buy += 1 to each one
-        self.sig_required_buy = 3
-        # Sells - currently EMA/WMA, MACDLead - self.pts_sig_required_sell += 1
-        self.sig_required_sell = 0 # set to 0 for default
+        # Buys - currently RSI, OBV - add self.pts_sig_required_buy += 1 to each one
+        self.sig_required_buy = 2
+        # Sells - currently EMA/WMA - self.pts_sig_required_sell += 1
+        self.sig_required_sell = 1 # set to 0 for default
 
         # start at 0
         self.buy_pts = 0
@@ -54,13 +54,13 @@ class Strategy_CS:
 
         # RSI with VWMA, percent RSI is above MA for strength
         if ( # Buy when RSI is increasing and above MA by 10%
-            data["rsi_ma_pcnt"].values[0] > 10
-            and data["rsi14_pc"].values[0] >= 0
+            data["rsi_ma_pcnt"].values[0] >= 10
         ):
-#            self.pts_sig_required_buy += 1
+            self.pts_sig_required_buy += 1
             if ( # Strong when RSI is 20% above MA and RSI > 50
                 data["rsi_ma_pcnt"].values[0] > 20
                 and data['rsi14'].values[0] > 50
+                and data["rsi14_pc"].values[0] >= 0
             ):
                 self.rsi_action = "strongbuy"
                 self.buy_pts += 2
@@ -68,11 +68,14 @@ class Strategy_CS:
                 self.rsi_action = "buy"
                 self.buy_pts += 1
         elif ( # Sell if RSI if decreasing
-            data["rsi14_pc"].values[0] < 0
+            data["rsi_ma_pcnt"].values[0] < 5
         ):
 #            self.pts_sig_required_sell += 1
             # Strong when RSI is more than 5% below MA
-            if data["rsi_ma_pcnt"].values[0] < -10:
+            if (
+                data["rsi_ma_pcnt"].values[0] < -10
+                and data["rsi14_pc"].values[0] < 0
+            ):
                 self.rsi_action = "strongsell"
                 self.sell_pts += 2
             else:
@@ -85,11 +88,10 @@ class Strategy_CS:
         if ( # DI+ above DI- and a difference of 20% and +DI increasing
             data["+di14"].values[0] > data["-di14"].values[0]
             and data["di_pcnt"].values[0] > 20
-            and data["+di_pc"].values[0] > 0
         ):
-            self.pts_sig_required_buy += 1
+#            self.pts_sig_required_buy += 1
             if ( # Strong if ADX is > 30, DI difference greater than 50% or DI+ is above ADX
-                data["adx14"].values[0 > 30]
+                data["adx14"].values[0] > 30
                 and (data["di_pcnt"].values[0] > 50
                     or  data["+di14"].values[0] > data["adx14"].values[0])
             ):
@@ -99,8 +101,7 @@ class Strategy_CS:
                 self.adx_action = "buy"
                 self.buy_pts += 1
         elif ( # Sell if +DI decreasing or DI+ is below DI-
-            (data["+di_pc"].values[0] < 0
-                or data["+di14"].values[0] < data["-di14"].values[0])
+            data["+di14"].values[0] < data["-di14"].values[0]
         ):
             if ( # Strong if DI difference is below -10% or DI- is above ADX
                 (data["di_pcnt"].values[0] < -10
@@ -158,7 +159,7 @@ class Strategy_CS:
             (data["obv_sm_diff"].values[0] < 0
                 or data['obv_pc'].values[0] < 0)
         ):
-            self.pts_sig_required_sell += 1
+#            self.pts_sig_required_sell += 1
             self.obv_action = "sell"
             self.sell_pts += 1
         else:
@@ -167,13 +168,12 @@ class Strategy_CS:
         # MACD Leader signal.....
         # for short trading in pycryptobot, we check that MacdLeader > Macdl_sig and upward trend
         if ( # MACDL above Signal by 40% and MACDL change > 20%
-            data["macdl_sg_diff"].values[0] > 50
-            and data["macdlead_pc"].values[0] > 20
-            and data["macdlead"].values[0] > 0
+            data["macdl_sg_diff"].values[0] > 40
+            and data["macdlead_pc"].values[0] > 30
         ):
-            self.pts_sig_required_buy += 1
+#            self.pts_sig_required_buy += 1
             if ( # Strong when MACDL changes 40% or more, MACD is 100% above Signal and MACDL is > 0
-            data["macdlead_pc"].values[0] > 40
+                data["macdlead"].values[0] > 0
                 and data["macdl_sg_diff"].values[0] > 100
             ):
                 self.macdl_action = "strongbuy"
@@ -184,7 +184,7 @@ class Strategy_CS:
         elif ( # Sell when MACDL Starts decreasing
             data["macdlead_pc"].values[0] < 0
         ):
-            self.pts_sig_required_sell += 1
+#            self.pts_sig_required_sell += 1
             if ( # Strong when MACDL below Signal or MACDL < 0
                 data["macdl_sg_diff"].values[0] < 0
             ):
@@ -199,9 +199,9 @@ class Strategy_CS:
         # EMA5/WMA5 crossover signal
         if ( # EMA above WMA
             data["ema5"].values[0] > data["ema5_wma5"].values[0]
-            and data["ema5_pc"].values[0] > 0
+            and data["ema5_pc"].values[0] > 1
         ):
-            self.pts_sig_required_buy += 1
+#            self.pts_sig_required_buy += 1
             if ( # Strong when EMA_pc > 8
                 data["ema5_pc"].values[0] > 5
             ):
@@ -211,7 +211,7 @@ class Strategy_CS:
                 self.emawma_action = "buy"
                 self.buy_pts += 1
         elif ( # Sell when EMA < WMA
-            data["ema5_pc"].values[0] < 0
+            data["ema5_pc"].values[0] < 1
         ):
             self.pts_sig_required_sell += 1
             if data["ema5"].values[0] < data["ema5_wma5"].values[0]:
